06d74de8b7838a2815b2833e564f0a043e28a83c
daniel.yonkov <daniel.yonkov@gmail.com>
2026-06-23 14:48:52 +0300

    2.6 Vectors -> 2.8 Exercises for vectors

--- R Basics/2.4-5 Data Types.R
+++ R Basics/2.4-5 Data Types.R
@@ -125,3 +125,146 @@ table(murders$region)
 
 #  Northeast         South    North Central          West 
 #         9            17                12            13 
+
+# 2.6 Vectors
+
+# 2.6.1 Creating vectors
+codes <- c(380, 124, 818)
+codes
+
+country <- c("Italy", "Canada", "Egypt")
+# or with single quotes
+country <- c('Italy', 'Canada', 'Egypt')
+
+# 2.6.2 Names
+codes <- c(italy = 380,
+           canada = 124,
+           egypt = 818)
+codes
+
+class(codes) # still numeric vector
+
+# with names
+names(codes)
+
+# we can use quotes for the names
+codes <- c("italy" = 380,
+           "canada" = 124,
+           "egypt" = 818)
+
+# assigning names
+codes <- c(380, 124, 818)
+country <- c("Italy", "Canada", "Egypt")
+names(codes) <- country
+codes
+
+# 2.6.3 Sequences
+seq(1, 10)
+
+seq(1, 10, 2)
+
+#shorthand
+1:10
+
+# 2.6.4 Subsetting
+codes[2]
+
+# accessing 1st and 3rd element
+codes[c(1, 3)]
+# accessing subsequent elements
+codes[1:2]
+# accessing using the name
+codes["Canada"]
+codes[c("Egypt", "Canada")]
+
+# 2.7 Coercion
+x <- c(1, "canada", 3) # no error
+class(x) # character - casted the values to character because it did not know
+# how to convert "canada" to numeric value
+
+x <- 1:5
+y <- as.character(x)
+
+y
+
+as.numeric(y)
+
+# 2.7.1 Not availables (NA)
+
+x <- c("1", "b", "3")
+as.numeric(x) # NOTICE the warning the appears for NA
+
+# 2.8 Exercises
+
+# 1. Use the function `c` to create a vector with the average high
+# temperatures in January for Beijing, Lagos, Paris, Rio de Janeiro,
+# San Juan, and Toronto, which are 35, 88, 42, 84, 81, and 30 degrees
+# Fahrenheit. Call the object temp.
+
+temp <- c(35, 88, 42, 84, 81, 30)
+
+# 2. Now create a vector with the city names and call the object city.
+city <- c("Beijing",
+          "Lagos",
+          "Paris",
+          "Rio de Janeiro",
+          "San Juan",
+          "Toronto")
+
+# 3. Use the names function and the objects defined in the previous exercises
+# to associate the temperature data with its corresponding city.
+names(temp) <- city
+temp
+
+#   Beijing          Lagos          Paris Rio de Janeiro       San Juan 
+#        35             88             42             84             81 
+# Toronto 
+#      30 
+
+# 4. Use the `[` and `:` operators to access the temperature of the first
+# three cities on the list.
+temp[1:3]
+
+# Beijing   Lagos   Paris 
+#      35      88      42 
+
+# 5. Use the`[` operator to access the temperature of Paris and San Juan.
+temp[c("Paris", "San Juan")]
+
+#    Paris San Juan 
+#       42       81 
+
+# 6. Use the `:` operator to create a sequence of numbers 12, 13, 14,..., 73.
+12:73
+
+# 7. Create a vector containing all the positive odd numbers smaller than 100.
+seq(1,100, 2)
+
+# 8. Create a vector of numbers that starts at 6, does not pass 55, 
+# and adds numbers in increments of 4/7: 6, 6 + 4/7, 6 + 8/7, and so on. 
+# How many numbers does the list have?
+# Hint: use seq and length.
+length(seq(6, 55, 4/7))
+# Answer: 86
+
+# 9. What is the class of the following object a <- seq(1, 10, 0.5)?
+a <- seq(1,10,0.5)
+class(a)
+# Answer: numeric
+
+# 10. What is the class of the following object a <- seq(1, 10)?
+a <- seq(1,10)
+class(a)
+# Answer: integer
+
+# 11. The class of class(a<-1) is numeric, not integer. R defaults to 
+# numeric and to force an integer, you need to add the letter L. 
+# Confirm that the class of 1L is integer.
+class(a <- 1L)
+
+# 12. Define the following vector:
+x <- c("1", "3", "5")
+
+# and coerce it to get integers.
+as.integer(x)
+
