083c9038d72ed2b5c0e0ea0ef05c54690bcea599
daniel.yonkov <daniel.yonkov@gmail.com>
2026-07-09 15:04:21 +0300

    Feat: add 4.7 and 4.8 sections exercises

--- 4. The tidyverse/4.7 Summarizing data and 4.8 Sorting data frames.R
+++ 4. The tidyverse/4.7 Summarizing data and 4.8 Sorting data frames.R
@@ -111,10 +111,81 @@ murders |>
 # 4.8.2 The `top` n
 
 # Note that rows are not sorted by rate, only filtered. If we want to sort, we need to use
-# `arrange`. Note that if the third argument is left blank, `top_n`, filters by the 
+# `arrange`. Note that if the third argument is left blank, `top_n`, filters by the
 # last column.
 murders |> top_n(5)
 
 # equivalent
-murders |> slice_min(rate, n=5) # with sorting in asc order
-murders |> slice_max(rate, n=5) # with sorting in desc order
+murders |> slice_min(rate, n = 5) # with sorting in asc order
+murders |> slice_max(rate, n = 5) # with sorting in desc order
+
+# 4.9 Exercises
+library(NHANES)
+data(NHANES)
+
+# 1. We will provide some basic facts about blood pressure. First let’s select a group to set
+# the standard. We will use 20-to-29-year-old females. `AgeDecade` is a categorical variable
+# with these ages. Note that the category is coded like ” 20-29“, with a space in front!
+# What is the average and standard deviation of systolic blood pressure as saved in the
+# `BPSysAve` variable? Save it to a variable called ref.
+
+# levels(NHANES$AgeDecade)
+ref <- NHANES |>
+  filter(AgeDecade == " 20-29") |>
+  summarise(avg = mean(BPSysAve, na.rm = TRUE),
+            sd = sd(BPSysAve, na.rm = TRUE))
+ref$avg # [1] 113.1583
+ref$sd # [1] 11.71519
+
+# 2. Using a pipe, assign the average to a numeric variable `ref_avg`. Hint: Use the code
+# similar to above and then pull.
+ref_avg <- ref |> pull(avg)
+
+# 3. Now report the min and max values for the same group.
+NHANES |>
+  filter(AgeDecade == " 20-29") |>
+  summarise(min = min(BPSysAve, na.rm = TRUE),
+            max = max(BPSysAve, na.rm = TRUE))
+
+#   min   max
+#    84   179
+
+# 4. Compute the average and standard deviation for females, but for each age group
+# separately rather than a selected decade as in question 1. Note that the age groups are
+# defined by AgeDecade. Hint:rather than filtering by `age` and `gender`, `filter` by
+# Gender and then use group_by.
+
+NHANES |>
+  filter(Gender == "female" & is.na(AgeDecade) != TRUE) |>
+  group_by(AgeDecade) |>
+  summarise(avg = mean(BPSysAve, na.rm = TRUE),
+            sd = sd(BPSysAve, na.rm = TRUE))
+
+# 5. Repeat exercise 4 for males.
+
+NHANES |>
+  filter(Gender == "male" & is.na(AgeDecade) != TRUE) |>
+  group_by(AgeDecade) |>
+  summarise(avg = mean(BPSysAve, na.rm = TRUE),
+            sd = sd(BPSysAve, na.rm = TRUE))
+
+# 6. We can actually combine both summaries for exercises 4 and 5 into one line of code.
+# This is because group_by permits us to group by more than one variable. Obtain one big
+# summary table using group_by(AgeDecade, Gender).
+
+NHANES |>
+  filter(is.na(AgeDecade) != TRUE) |>
+  group_by(AgeDecade, Gender) |>
+  summarise(avg = mean(BPSysAve, na.rm = TRUE),
+            sd = sd(BPSysAve, na.rm = TRUE))
+
+# 7. For males between the ages of 40-49, compare systolic blood pressure across race as
+# reported in the Race1 variable. Order the resulting table from lowest to highest average
+# systolic blood pressure.
+
+NHANES |>
+  filter(Gender == "male"  & AgeDecade == " 40-49") |>
+  group_by(Race1) |>
+  summarise(avg = mean(BPSysAve, na.rm = TRUE)) |>
+  arrange(avg) |>
+  mutate(gender = "male")
