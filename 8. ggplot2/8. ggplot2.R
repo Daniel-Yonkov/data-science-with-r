985dc28b4047c498d40c13402cc1c46a650551d2
daniel.yonkov <daniel.yonkov@gmail.com>
2026-07-27 12:32:20 +0300

    Feat: add a simple ggplot mapping with aestetics

--- 8. ggplot2/8. ggplot2.R
+++ 8. ggplot2/8. ggplot2.R
@@ -5,17 +5,17 @@ library(ggplot2)
 # target: build a plot similar to the one in ggplot-example-plot-1.png
 
 # The main three components to note are:
-  # Data: The US murders data frame is being summarized
-  # Geometry: The plot in the example is a scatterplot. This is referred to as the geometry component.
-  # Aesthetic mapping: The plot uses several visual cues to represent the information provided by the dataset. 
-  # The two most important cues in this plot are the point positions on the x-axis and y-axis, which represent population size 
-  # and the total number of murders, respectively. Each point represents a different observation, and we map data about these 
-  # observations to visual cues like x- and y-scale. Color is another visual cue that we map to region. We refer to this as the
-  # aesthetic mapping component. How we define the mapping depends on what geometry we are using.
+# Data: The US murders data frame is being summarized
+# Geometry: The plot in the example is a scatterplot. This is referred to as the geometry component.
+# Aesthetic mapping: The plot uses several visual cues to represent the information provided by the dataset.
+# The two most important cues in this plot are the point positions on the x-axis and y-axis, which represent population size
+# and the total number of murders, respectively. Each point represents a different observation, and we map data about these
+# observations to visual cues like x- and y-scale. Color is another visual cue that we map to region. We refer to this as the
+# aesthetic mapping component. How we define the mapping depends on what geometry we are using.
 
 # general structure
 # DATA |> ggplot() + LAYER 1 + LAYER 2 + … + LAYER N
-# Layers can define geometries, compute summary statistics, define what scales to use, or change styles. To add layers, we use 
+# Layers can define geometries, compute summary statistics, define what scales to use, or change styles. To add layers, we use
 # the symbol `+`
 
 # --------------------------
@@ -46,3 +46,9 @@ p
 
 # --------------------------
 
+# 8.4 Aesthetic mappings
+# Aesthetic mappings describe how properties of the data connect with features of the graph, such as distance along an axis,
+# size, or color.
+# The `aes` function connects data with what we see on the graph by defining aesthetic mappings.
+
+murders |> ggplot() + geom_point(aes(population/10^6, total))
