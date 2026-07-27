93662c0ce9042de38f622883b9d693f02b8bd831
daniel.yonkov <daniel.yonkov@gmail.com>
2026-07-27 12:28:47 +0300

    Feat: add first 3 sections in ggplot chapter

---
+++ 8. ggplot2/8. ggplot2.R
@@ -0,0 +1,48 @@
+library(dplyr)
+library(ggplot2)
+
+# 8.1 The components of a graph
+# target: build a plot similar to the one in ggplot-example-plot-1.png
+
+# The main three components to note are:
+  # Data: The US murders data frame is being summarized
+  # Geometry: The plot in the example is a scatterplot. This is referred to as the geometry component.
+  # Aesthetic mapping: The plot uses several visual cues to represent the information provided by the dataset. 
+  # The two most important cues in this plot are the point positions on the x-axis and y-axis, which represent population size 
+  # and the total number of murders, respectively. Each point represents a different observation, and we map data about these 
+  # observations to visual cues like x- and y-scale. Color is another visual cue that we map to region. We refer to this as the
+  # aesthetic mapping component. How we define the mapping depends on what geometry we are using.
+
+# general structure
+# DATA |> ggplot() + LAYER 1 + LAYER 2 + … + LAYER N
+# Layers can define geometries, compute summary statistics, define what scales to use, or change styles. To add layers, we use 
+# the symbol `+`
+
+# --------------------------
+
+# 8.2 Initializing an object with data
+library(dslabs)
+data(murders)
+
+ggplot(data = murders)
+# equivalence
+murders |> ggplot()
+# we see only gray background because no geometry has been selected and the default is used (gray background)
+
+p <- ggplot(data = murders)
+
+print(p)
+# equivalence
+p
+
+# --------------------------
+
+# 8.3 Adding a geometry
+# use ggplot2-cheatsheets.png for reference
+# geometry function names follow the pattern: `geom_X` where X is the name of the geometry.
+# for scatterplot, we need `geom_point`
+?geom_point
+# at the Aesthetics section we see what are the required parameters in bold
+
+# --------------------------
+
--- README.md
+++ README.md
@@ -65,4 +65,7 @@ Below you will find links related to the book structure and code examples. Those
   - [7.5 Stratification](https://rafalab.dfci.harvard.edu/dsbook-part-1/dataviz/distributions.html#sec-boxplots)
   - [7.6 Case study continued](https://rafalab.dfci.harvard.edu/dsbook-part-1/dataviz/distributions.html#sec-student-height-cont)
   - [7.7 Exercises](./7.%20Visualizing%20data%20distributions/7.7%20Exercises.R#L1)
-- [8  ggplot2](https://rafalab.dfci.harvard.edu/dsbook-part-1/dataviz/ggplot2.html) TODO
+- [8. ggplot2](https://rafalab.dfci.harvard.edu/dsbook-part-1/dataviz/ggplot2.html)
+  - [8.1 The components of a graph](./8.%20ggplot2/8.%20ggplot2.R#L4)
+  - [8.2 Initializing an object with data](./8.%20ggplot2/8.%20ggplot2.R#L23)
+  - [8.3 Adding a geometry](./8.%20ggplot2/8.%20ggplot2.R#L40)
